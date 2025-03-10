ed2e50b0669b4b480da59dc47279eb8aefa718b2
Haoda Fan <haoda.fan@outlook.com>
2025-03-10 18:36:15 -0400

    Added batch script and gitignore

---
+++ update-factorio.bat
@@ -0,0 +1,15 @@
+echo off
+
+SET appDataPath=%APPDATA%\Factorio\mods\Airport-Logistics-Factorio
+IF NOT EXIST "%appDataPath%" MKDIR "%appDataPath%"
+
+echo Copying files and subfolders to the Factorio mods directory...
+ROBOCOPY "%cd%" "%appDataPath%" /E /XD "update-factorio.bat" > "%cd%\update-factorio-batch-log.txt"
+
+IF ERRORLEVEL 8 (
+    echo An error occurred while copying files. Check update-factorio-batch-log.txt for details.
+) ELSE (
+    echo Files and subfolders copied successfully. Check update-factorio-batch-log.txt for details.
+)
+
+PAUSE
