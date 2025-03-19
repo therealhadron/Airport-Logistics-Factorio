f98adbec4abb4def7f8113c3dcebfab612a22bc1
therealhadron <therealhadron@pm.me>
2025-03-18 22:29:17 -0400

    added minimap to airport manager with zoom in and zoom out

--- data.lua
+++ data.lua
@@ -13,9 +13,19 @@ styles["apl_scroll_pane"] = {
 
 data:extend({
     {
-        type= "custom-input",
+        type = "custom-input",
         name = "apl_toggle_airport_manager",
         key_sequence = "CONTROL + P",
         order = "a"
+    },
+    {
+        type = "custom-input",
+        name = "apl_minimap_zoom_in",
+        key_sequence = "mouse-wheel-up"
+    },
+    {
+        type = "custom-input",
+        name = "apl_minimap_zoom_out",
+        key_sequence = "mouse-wheel-down"
     }
 })
--- scripts/gui.lua
+++ scripts/gui.lua
@@ -1,3 +1,41 @@
+local function build_airport_object(parent, caption)
+    local frame = parent.add{
+        type = "frame",
+        name = "airport_object_frame" .. caption,
+        style = "apl_content_frame"
+    }
+    local airport_name = frame.add{
+        type = "label",
+        name = "label_airport_name_" .. caption,
+        caption = caption
+    }
+    airport_name.style.right_padding = 100
+    local line = frame.add{
+        type = "line",
+        name = "line_" .. caption,
+        direction = "vertical"
+    }
+    local statuses = frame.add{
+        type = "table",
+        column_count = 1
+    }
+    local status1 = statuses.add{
+        type = "label",
+        name = "label_status1_" .. caption,
+        caption = "Status: " .. caption
+    }
+    local status2 = statuses.add{
+        type = "label",
+        name = "label_status2_" .. caption,
+        caption = "Status: " .. caption
+    }
+    local status3 = statuses.add{
+        type = "label",
+        name = "label_status3_" .. caption,
+        caption = "Status: " .. caption
+    }
+end
+
 local function build_airport_manager_interface(player)
     local player_storage = storage.players[player.index]
     local frame = player.gui.screen
@@ -14,7 +52,7 @@ local function build_airport_manager_interface(player)
     -- Minimap frame
     local map_frame = main_frame_airport_manager.add{
         type = "frame",
-        name = "map_frame",
+        name = "apl_minimap_frame",
         direction = "vertical",
         style = "apl_content_frame"
     }
@@ -22,23 +60,15 @@ local function build_airport_manager_interface(player)
     map_frame.style.horizontally_stretchable = false
     map_frame.style.vertically_stretchable = true
 
-    -- Minimap
-    -- local minimap_button = content_frame.add
-    -- {
-    --   type = "button",
-    --   style = "locomotive_minimap_button"
-    -- }
-    -- minimap_button.style.width = 176
-    -- minimap_button.style.height = 176
     local minimap = map_frame.add{
         type = "minimap",
+        name = "apl_minimap",
         position = {0,0},
-        zoom = 0.25
     }
-    minimap.style.minimal_width = 176
-    minimap.style.minimal_height = 176
     minimap.style.horizontally_stretchable = true
     minimap.style.vertically_stretchable = true
+    player_storage.elements.minimap = minimap
+    minimap.zoom = player_storage.zoom_level
 
     -- Airport list frame
     local airport_list_frame = main_frame_airport_manager.add{
@@ -48,7 +78,6 @@ local function build_airport_manager_interface(player)
         style = "apl_content_frame"
     }
     airport_list_frame.style.minimal_width = 250
-    airport_list_frame.style.minimal_height = 500
     airport_list_frame.style.horizontally_stretchable = true
     airport_list_frame.style.vertically_stretchable = true
 
@@ -58,7 +87,18 @@ local function build_airport_manager_interface(player)
         style = "apl_scroll_pane"
     }
     airport_list.style.horizontally_stretchable = true
-    airport_list.style.vertically_stretchable = true
+    airport_list_frame.style.vertically_stretchable = true
+
+    local table = airport_list.add {
+        type = "table",
+        name = "airport_table",
+        column_count = 1
+    }
+
+    local objects = {"YYZ", "YVR", "NYC", "NRT", "LAX", "HND", "SFO"} -- filler
+    for _, objects in pairs(objects) do
+        build_airport_object(table, objects)
+    end
 
     player.opened = main_frame_airport_manager
     player_storage.elements.main_frame_airport_manager = main_frame_airport_manager
@@ -67,7 +107,7 @@ end
 local function toggle_airport_manager(player)
     local player_storage = storage.players[player.index]
     local main_frame_airport_manager = player_storage.elements.main_frame_airport_manager
-    
+
     if main_frame_airport_manager == nil then
         build_airport_manager_interface(player)
     else
@@ -77,7 +117,7 @@ local function toggle_airport_manager(player)
 end
 
 local function initialize_storage(player)
-    storage.players[player.index] = { elements = {} }
+    storage.players[player.index] = { elements = {}, zoom_level = 10, zoom = true }
 end
 
 script.on_init(function()
@@ -121,5 +161,45 @@ script.on_event(defines.events.on_gui_closed, function(event)
     if event.element and event.element.name == "apl_main_frame_airport_manager" then
         local player = game.get_player(event.player_index)
         toggle_airport_manager(player)
+    end
+end)
+
+script.on_event(defines.events.on_gui_hover, function(event)
+    if event.element.name == "apl_minimap" then
+        local player_storage = storage.players[event.player_index]
+        player_storage.zoom = true
+        game.print("hovering")      
+    end
+end)
+
+script.on_event(defines.events.on_gui_leave, function(event)
+    if event.element.name == "apl_minimap" then
+        local player_storage = storage.players[event.player_index]
+        player_storage.zoom = false
+        game.print("not hovering")
+    end
+end)
+
+script.on_event("apl_minimap_zoom_in", function(event)
+    local player_storage = storage.players[event.player_index]
+    local main_frame_airport_manager = player_storage.elements.main_frame_airport_manager
+    if main_frame_airport_manager ~= nil then
+        local minimap = player_storage.elements.minimap
+        if player_storage.zoom_level < 1000 then
+            player_storage.zoom_level = player_storage.zoom_level * 1.1
+        end
+        minimap.zoom = player_storage.zoom_level
+    end
+end)
+
+script.on_event("apl_minimap_zoom_out", function(event)
+    local player_storage = storage.players[event.player_index]
+    local main_frame_airport_manager = player_storage.elements.main_frame_airport_manager
+    if main_frame_airport_manager ~= nil then
+        local minimap = player_storage.elements.minimap
+        if player_storage.zoom_level > 1 then
+            player_storage.zoom_level = player_storage.zoom_level * 0.9
+        end
+        minimap.zoom = player_storage.zoom_level
     end
 end)
