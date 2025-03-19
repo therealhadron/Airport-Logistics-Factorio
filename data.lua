e5013db93aeef5572c9b0a3b690d6ba995c9bdbd
therealhadron <therealhadron@pm.me>
2025-03-18 22:44:19 -0400

    moved custom inputs and styles to new gui folder

--- data.lua
+++ data.lua
@@ -1,31 +1,3 @@
-local styles = data.raw["gui-style"].default
-styles["apl_content_frame"] = {
-    type = "frame_style",
-    parent = "inside_shallow_frame_with_padding",
-    vertically_stretchable = "on"
-}
-
-styles["apl_scroll_pane"] = {
-    type = "scroll_pane_style",
-    parent = "scroll_pane",
-    vertically_stretchable = "on"
-}
-
-data:extend({
-    {
-        type = "custom-input",
-        name = "apl_toggle_airport_manager",
-        key_sequence = "CONTROL + P",
-        order = "a"
-    },
-    {
-        type = "custom-input",
-        name = "apl_minimap_zoom_in",
-        key_sequence = "mouse-wheel-up"
-    },
-    {
-        type = "custom-input",
-        name = "apl_minimap_zoom_out",
-        key_sequence = "mouse-wheel-down"
-    }
-})
+require ("prototypes.entity.airporthub")
+require ("gui.styles")
+require ("gui.inputs")
